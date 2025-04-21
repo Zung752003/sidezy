ef92cdcb1edf9079044bf07d961f6f30a98c1c94
Nguyen Hoang <chungtawf@gmail.com>
2025-04-21 15:42:34 +0700

    Handle loop infinity

--- src/slidezy.js
+++ src/slidezy.js
@@ -1,56 +1,90 @@
-function Slidezy(selector, options = {}){
+function Slidezy(selector, options = {}) {
     this.container = document.querySelector(selector);
-    if(!this.container){
+    if (!this.container) {
         console.error(`Slidezy: Container '${selector} not found!'`);
         return;
     }
 
-    this.options = Object.assign({},options);
-    this.slide = Array.from(this.container.children);
-    this.currentIndex = 0;
-
+    this.opt = Object.assign({
+        items: 1,
+        speed: 300,
+        loop: true,
+    }, options);
+    this.slides = Array.from(this.container.children);
+    this.currentIndex = this.opt.loop ? this.opt.items : 0;
 
     this._init();
+    this._updatePosition();
 }
 
-Slidezy.prototype._init = function(){
+Slidezy.prototype._init = function () {
     this.container.classList.add("slidezy-wrapper");
 
     this._creatTrack();
     this._createNavigation();
 }
 
-Slidezy.prototype._creatTrack = function(){
+Slidezy.prototype._creatTrack = function () {
     this.track = document.createElement("div");
-    this.track.className ="slidezy-track"
-    
-    this.slide.forEach((slide) => {
-        slide.classList.add("slidezy-slide")
-        this.track.append(slide)
+    this.track.className = "slidezy-track";
+
+    if(this.opt.loop){
+        const cloneHead = this.slides.slice(-this.opt.items).map(node => node.cloneNode(true));
+        const cloneTail = this.slides.slice(0, this.opt.items).map(node => node.cloneNode(true));
+
+        //4 5 6 1 2 3 4 5 6 1 2 3
+        this.slides = cloneHead.concat(this.slides.concat(cloneTail));
+    }
+
+    this.slides.forEach((slide) => {
+        slide.classList.add("slidezy-slide");
+        slide.style.flexBasis = `calc(100% / ${this.opt.items})`;
+        this.track.append(slide);
     });
-    
+
     this.container.appendChild(this.track);
 }
 
-Slidezy.prototype._createNavigation = function(){
+Slidezy.prototype._createNavigation = function () {
     this.prevBtn = document.createElement("button");
     this.nextBtn = document.createElement("button");
-    
+
     this.prevBtn.textContent = "<";
     this.nextBtn.textContent = ">";
-    
+
     this.prevBtn.className = "slidezy-prev";
     this.nextBtn.className = "slidezy-next";
-    
+
     this.container.append(this.prevBtn, this.nextBtn);
 
     this.prevBtn.onclick = () => this.moveSlide(-1);
     this.nextBtn.onclick = () => this.moveSlide(1);
 }
 
-Slidezy.prototype.moveSlide = function(step){
-    this.currentIndex = Math.min(Math.max(this.currentIndex + step, 0), this.slide.length - 3);
+Slidezy.prototype.moveSlide = function (step) {
+    if(this._isAnimating) return;
+    this._isAnimating = true;
+
+    const maxIndex = this.slides.length - this.opt.items;
+
+    this.currentIndex = Math.min(Math.max(this.currentIndex + step, 0), maxIndex);
     
-    this.offset = -(this.currentIndex * (100/3));
+    setTimeout(() => {
+        if (this.opt.loop) {
+            if(this.currentIndex <= 0) {
+                this.currentIndex = maxIndex - this.opt.items;
+            } else if (this.currentIndex >= maxIndex) {
+                this.currentIndex = this.opt.items;
+            }
+            this._updatePosition(true);
+        }
+        this._isAnimating = false;
+    }, this.opt.speed)
+    this._updatePosition();
+}
+
+Slidezy.prototype._updatePosition = function (instant = false) {
+    this.track.style.transition = instant ? "none" : `transform ease ${this.opt.speed}ms`;
+    this.offset = -(this.currentIndex * (100 / this.opt.items));
     this.track.style.transform = `translateX(${this.offset}%)`;
 }
